2d2dc37d615f9439a19f73be11cd818289538355
Yiannis Chambers <Yiannis.Chambers@student.uts.edu.au>
2016-05-16 17:07:07 +1000

    Created project, and Main Menu Activity.
    - Created new Android Studio project.
    
    - Added Main Menu Activity.
    
    - Removed toolbar and floating action button.
    
    - Added game title text, Start New Button and (invisible) Continue
    Button.

---
+++ Geopolitique/app/src/androidTest/java/geopolitique/id11699156/com/geopolitique/ApplicationTest.java
@@ -0,0 +1,13 @@
+package geopolitique.id11699156.com.geopolitique;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
